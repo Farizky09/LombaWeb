d89c9a5032e1ccac8280b5d0118160aaba924ea2
LAPTOP-HKETPD9A\hp <waridhil@gmail.com>
2022-08-24 13:45:14 +0700

    fix dasboard simple

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -47,7 +47,7 @@
       <div class="col-12">
         <div class="card mb-4">
           <div class="card-header pb-0">
-                <a href="/siswa/create"><button class="btn btn-primary float-end"> Tambah data</button></a>
+                <a href="/siswa/create"><button class="btn btn-primary float-end">Tambah data</button></a>
                 <h6>Data Siswa</h6>
           </div>    
           <div class="card-body px-0 pt-0 pb-2">
@@ -76,7 +76,7 @@
                       </div>
                     </td> --}}
                     <td>
-                      <p class="text-xs font-weight-bold mb-0">{{ $s->nama }}</p>                    
+                      <p class="text-xs font-weight-bold mb-0 ps-3">{{ $s->nama }}</p>                    
                     </td>
                     <td class="align-middle text-center text-sm">
                       <span class="text-secondary text-xs font-weight-bold">{{ $s->nis }}</span>
@@ -93,31 +93,15 @@
                     <td class="align-middle text-center">
                       <span class="text-secondary text-xs font-weight-bold">{{ $s->alamat }}</span>
                     </td>
-                    <td class="align-middle">
-                        <a href="/siswa/{{ $s->id }}/edit" class="badge badge-sm bg-gradient-warning mx-1" data-toggle="tooltip" data-original-title="Edit user">
-                            Edit
-                        </a>                                                     
-                    </td>
-                    <td>
+                 
+                    <td class="align-middle text-center">
                         <form action="/siswa/{{ $s->id }}" method="POST">
                             @csrf
                            @method('delete')
-                           <input class="mx-1" style="background-image: linear-gradient(310deg, #fb6340 0%, #fb4040 100%); --bs-badge-padding-x: 0.9em;
-                           --bs-badge-padding-y: 0.55em;
-                           --bs-badge-font-size: 0.75em;
-                           --bs-badge-font-weight: 700;
-                           --bs-badge-color: #fff;
-                           --bs-badge-border-radius: 0.45rem;
-                           display: inline-block;
-                           padding: var(--bs-badge-padding-y) var(--bs-badge-padding-x);
-                           font-size: var(--bs-badge-font-size);
-                           font-weight: var(--bs-badge-font-weight);
-                           line-height: 1;
-                           color: var(--bs-badge-color);
-                           text-align: center;
-                           white-space: nowrap;
-                           vertical-align: baseline;
-                           border-radius: var(--bs-badge-border-radius, 0);" type="submit" onclick="return confirm('apakah anda yakin ingin dihapus?')" value="Delete">
+                           <input class="btn btn-danger btn-sm mx-1 mt-3" type="submit" onclick="return confirm('apakah anda yakin ingin dihapus?')" value="Delete">
+                            <a href="/siswa/{{ $s->id }}/edit" class="btn btn-warning btn-sm mx-1 mt-3" data-toggle="tooltip" data-original-title="Edit user">
+                              Edit
+                          </a>   
                        </form>
                     </td>
                   </tr>
