45b7980b1ba4a5f238a0cfe36a7aa390280d6255
DESKTOP-VROB9DB\LENOVO <ahmadfarizky26082004@gmail.com>
2022-08-25 07:34:55 +0700

    no message

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -70,7 +70,7 @@
                     <td>
                       <div class="d-flex px-2 py-1">                      
                         <div class="d-flex flex-column justify-content-center">
-                          <h6 class="mb-0 text-sm">{{ $s->id }}</h6>                          
+                          <h6 class="mb-0 text-sm">{{ $s->nis }}</h6>                          
                         </div>
                       </div>
                     </td>
