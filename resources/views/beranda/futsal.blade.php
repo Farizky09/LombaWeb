43e4e5cec0c157ece756a02b8e58c93d3b4ed4ee
DESKTOP-VROB9DB\LENOVO <ahmadfarizky26082004@gmail.com>
2022-08-31 11:45:57 +0700

    ADD

--- resources/views/beranda/futsal.blade.php
+++ resources/views/beranda/futsal.blade.php
@@ -1,2 +1,46 @@
-<h1>Test</h1>
+<h1><center>FUTSAL</h1></center><hr>
+
+<table >
+    <tbody>
+        <tr><h4>BIODATA</h4>
+    <tr><td>Nama CLUB</td><td>:</td><td>FUTSAL SKANAMBER</td></tr>
+    <tr><td>Pelatih</td><td>:</td><td>Pak RIDWAN</td></tr>
+    <tr><td>Hari Latihan</td><td>:</td><td>Senin</td></tr>
+    <tr><td>Jam</td><td>:</td><td>15.00 - selesai</td></tr>
+</tr>
+ 
+
+    </tbody>
+</table>
+<tr><h4>VISI</h4>
+<tr>
+    <p>Kegiatan ekstrakurikuler futsal adalah mengembangkan potensi, bakat serta minat siswa terhadap olahragayang diminatinya
+        secara optimal, serta menumbuh kembangkan kemandirian, kerjasama, nilai sosial serta menjunjung tinggi nilai-ilai sportivitas
+        yang akan berguna untuk dirinya, keluarga dan masyarakat
+    </p>
+</tr>
+</tr>
+
+<tr><h4>MISI</h4>
+    <tr>
+        <p>
+            &#45;	Membuat kekeluargaan sesama siswa SMKN 6 JEMBER lewat futsal. 
+        </p>
+        <p>
+            &#45;	Menjalin silaturahim dengan sekolah lain dengan futsal. 
+        </p>
+        <p>
+            &#45;	 Menjadikan cara bermain futsal yang baik dan benar.
+        </p>
+        <p>
+            &#45;	Menjadikan olahraga futsal sebagai penyalur hobi yang baik.
+        </p>
+    </tr>
+    </tr>
+
+
+
+
+
+
 <a href="/beranda" >back</a>
--- resources/views/beranda/pmr.blade.php
+++ resources/views/beranda/pmr.blade.php
@@ -1,2 +1,48 @@
-<h1>Test</h1>
+<h1><center>Palang Merah Remaja</h1></center><hr>
+
+<table >
+    <tbody>
+       <tr></tr><h4>BIODATA</h4></tr>
+    <tr><td>Nama CLUB</td><td>:</td><td>PMR WIRA SMKN 6 JEMBER</td></tr>
+    <tr><td>Pelatih</td><td>:</td><td> Kak Candra aji & Kak Ali </td></tr>
+    <tr><td>Hari Latihan</td><td>:</td><td>Kamis</td></tr>
+    <tr><td>Jam</td><td>:</td><td>15.00-16.30</td></tr>
+
+
+    </tbody>
+</table>
+<tr><h4>VISI</h4>
+<tr>
+    <p>Membentuk PMR Wira SMKN 6 Jember yang siap menjalankan kegiatan sosial kemanusiaan, menjunjung tinggi 
+        Prinsip Dasar Palang Merah Indonesia, 
+        melaksanakan Tribakti PMR dengan baik, dan menjadikan PMR sebagai organisasi yang dicintai masyarakat.
+    </p>
+</tr>
+</tr>
+
+<tr><h4>MISI</h4>
+    <tr>
+        <p>
+            &#45;	Membentuk PMR sebagai jembatan untuk memberikan pelayanan dan menyalurkan rasa kemanusiaan kepada masyarakat 
+        </p>
+        <p>
+            &#45;	Mendidik anggota PMR agar memiliki jiwa kemanusiaan yang tinggi sehingga bisa menjadi calon-calon relawan di masa depan nanti 
+        </p>
+        <p>
+            &#45;	Melatih para anggota PMR agar selalu siap memberikan pertolongan di lingkungan sekolah
+        </p>
+        <p>
+            &#45;	Memberi motivasi untuk teman sebaya mengenai pola hidup sehat dan pentingnya menjaga kesehatan
+        </p>
+        <p>
+            &#45;	Melakukan yang terbaik untuk sesama tanpa mengharapkan imbalan
+        </p>
+    </tr>
+    </tr>
+
+
+
+
+
+
 <a href="/beranda" >back</a>
